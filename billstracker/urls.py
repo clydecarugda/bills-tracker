05d0bf5ef96b8fb05e22c5a11ba187b6981b3abb
Clyde Oliver Carugda <clydeocarugda@gmail.com>
2025-01-14 02:11:55 +0800

    "Added views and urls for the login page"

--- billstracker/urls.py
+++ billstracker/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-
+from .views import LoginPage
 
 
 urlpatterns = [
-    
+    path('login/', LoginPage.as_view(), name='login'),
 ]
--- billstracker/views.py
+++ billstracker/views.py
@@ -1,2 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
 
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login
+
+from .models import Bill
+
+
+class LoginPage(LoginView):
+  template_name = 'templates/login.html'
+  fields = '__all__'
+  redirect_authenticated_user = True
+  
+  def get_success_url(self):
+    return reverse_lazy('')
