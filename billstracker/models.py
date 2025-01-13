c3e708be1f8fd89577d79ec5b655a7e2fe9d1f9f
Clyde Oliver Carugda <clydeocarugda@gmail.com>
2025-01-14 02:04:23 +0800

    Added Models and migrations

--- billstracker/models.py
+++ billstracker/models.py
@@ -9,8 +9,8 @@ class PaymentStatus(models.Model):
     return self.name
   
 
-class Bills(models.Model):
-  user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+class Bill(models.Model):
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
   name = models.CharField(max_length=30)
   bill_type = models.CharField(max_length=25, null=True, blank=True)
   description = models.TextField(null=True, blank=True)
@@ -18,9 +18,28 @@ class Bills(models.Model):
   amount = models.FloatField(default=0)
   payment_status = models.ForeignKey('PaymentStatus', on_delete=models.CASCADE)
   recurring = models.BooleanField(default=False)
+  bill_month = models.IntegerField(choices=[(i, i) for i in range (1, 13)], verbose_name='Month')
   
   def __str__(self):
     return self.name
   
   class Meta:
     ordering = ['due_date']
+    
+
+class PaymentMethod(models.Model):
+  bill_id = models.ForeignKey(Bill, on_delete=models.CASCADE)
+  method_name = models.CharField(max_length=25)
+  amount = models.FloatField()
+  fee_amount = models.FloatField(default=0)
+  
+  def __str__(self):
+    return self.method_name
+  
+
+class USettings(models.Model):
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  auto_recurring = models.BooleanField(default=False)
+  
+  def __str__(self):
+    return self.auto_recurring
