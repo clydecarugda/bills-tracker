5e73256bc84134509078340e2f85e7240b443ee2
Clyde Oliver Carugda <clydeocarugda@gmail.com>
2025-01-14 13:49:32 +0800

    "Added design for bills tracker page"
    "Corrected bootstrap 5 links in main.html"
    "Added current Month view in views.py"

--- billstracker/views.py
+++ billstracker/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic.list import ListView
+import datetime
 
+from django.views.generic.list import ListView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login
@@ -32,5 +33,7 @@ class BillList(LoginRequiredMixin, ListView):
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
     context['bills'] = context['bills'].filter(user_id=self.request.user)
+    
+    context['current_month'] = datetime.datetime.now().strftime('%B')
     
     return context
