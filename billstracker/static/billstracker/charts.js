f1bcc917f17891511288ad8c47f83fdd14302e16
Clyde Oliver Carugda <clydeocarugda@gmail.com>
2025-02-27 14:54:16 +0800

    "Add dashboard View"
    "Add Upcoming Bills UI and functions"
    "Add Monthly Expense Breakdown Chart and functions"
    "Add Income vs Expense Chart and functions"
    "Add Expense Trend Chart and functions"
    "Add ajax to refresh chart data without reloading the page"

---
+++ billstracker/static/billstracker/charts.js
@@ -0,0 +1,172 @@
+var colorPalette = [
+  "#25CCF7","#FD7272","#54a0ff","#00d2d3",
+  "#1abc9c","#2ecc71","#3498db","#9b59b6","#34495e",
+  "#16a085","#27ae60","#2980b9","#8e44ad","#2c3e50",
+  "#f1c40f","#e67e22","#e74c3c","#ecf0f1","#95a5a6",
+  "#f39c12","#d35400","#c0392b","#bdc3c7","#7f8c8d",
+  "#55efc4","#81ecec","#74b9ff","#a29bfe","#dfe6e9",
+  "#00b894","#00cec9","#0984e3","#6c5ce7","#ffeaa7",
+  "#fab1a0","#ff7675","#fd79a8","#fdcb6e","#e17055",
+  "#d63031","#feca57","#5f27cd","#54a0ff","#01a3a4"
+];
+
+$(document).ready(function () {
+  var monthlyExpenseUrl = $("#monthly-expense-url").data("url");
+  var incomeExpenseUrl = $("#income-expense-url").data("url");
+  var expenseTrendsUrl = $("#expense-trend-url").data("url");
+
+  var monthlyExpense = document.getElementById("monthlyExpenseBreakdown").getContext("2d");
+  var incomeExpense = document.getElementById("incomeExpenseBreakdown").getContext("2d");
+  var expenseTrend = document.getElementById("ExpenseTrendBreakdown").getContext("2d");
+
+  const noDataPlugin = {
+    id: 'noData',
+    beforeDraw: function (chart) {
+      let hasData = chart.data.datasets.some(dataset => dataset.data.length > 0 && dataset.data.some(value => value > 0));
+
+      if (!hasData) {
+        const ctx = chart.ctx;
+        const { width, height } = chart;
+
+        ctx.save();
+        ctx.font = '16px Arial';
+        ctx.fillStyle = 'gray';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText('No Data Available', width / 2, height / 2);
+        ctx.restore();
+      }
+    }
+  };  
+
+  var monthlyExpenseChart = new Chart(monthlyExpense, {
+    type: 'pie',
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'Monthly Expense',
+        data: [],
+        backgroundColor: colorPalette,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true
+    },
+    plugins: [noDataPlugin]
+  });
+
+  var incomeExpenseChart = new Chart(incomeExpense, {
+    type: "bar",
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'Income Expense',
+        data: [],
+        backgroundColor: colorPalette,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+    plugins: [noDataPlugin]
+  });
+
+  var expenseTrendChart = new Chart(expenseTrend, {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'Expense Trends',
+        data: [],
+        backgroundColor: colorPalette,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true
+    },
+    plugins: [noDataPlugin]
+  });
+
+  function fetchMonthlyExpenseData(month = '') {
+    $.ajax({
+      url: monthlyExpenseUrl,
+      data: { month: month },
+      dataType: "json",
+      success: function (data) {
+        monthlyExpenseChart.data.labels = [];
+        monthlyExpenseChart.data.datasets[0].data = [];
+
+        monthlyExpenseChart.data.labels = data.labels;
+        monthlyExpenseChart.data.datasets[0].data = data.values;
+        monthlyExpenseChart.update();
+      }
+    });
+  }
+
+  function fetchIncomeExpenseData(month = '') {
+    $.ajax({
+      url: incomeExpenseUrl,
+      data: { month: month },
+      dataType: "json",
+      success: function (data) {
+        incomeExpenseChart.data.labels = [];
+        incomeExpenseChart.data.datasets[0].data = [];
+
+        incomeExpenseChart.data.labels = data.labels;
+        incomeExpenseChart.data.datasets[0].data = data.values;
+        incomeExpenseChart.update();
+      }
+    })
+  }
+
+  function fetchExpenseTrendData(value = '') {
+    $.ajax({
+      url: expenseTrendsUrl,
+      data: { option: value },
+      dataType: "json",
+      success: function (data) {
+        expenseTrendChart.data.labels = [];
+        expenseTrendChart.data.datasets[0].data = [];
+
+        console.log("Expense Trend Data:", data)
+
+        expenseTrendChart.data.labels = data.labels;
+        expenseTrendChart.data.datasets[0].data = data.values;
+        expenseTrendChart.update();
+      }
+    })
+  }
+
+  fetchMonthlyExpenseData($("#id_monthly_expense_month").val());
+  fetchIncomeExpenseData($('#id_income_expense_month').val());
+  fetchExpenseTrendData($('#id_expense_trend_selection').val());
+
+  $("#id_monthly_expense_month_filter").click(function () {
+    var selectedMonth = $("#id_monthly_expense_month").val();
+
+    fetchMonthlyExpenseData(selectedMonth)
+  });
+
+  $("#id_income_expense_month_filter").click(function () {
+    var selectedMonth = $("#id_income_expense_month").val();
+
+    fetchIncomeExpenseData(selectedMonth);
+  })
+
+  $("#id_expense_trend_filter").click(function () {
+    var selectedOption = $("#id_expense_trend_selection").val();
+
+    console.log("Expense Trend Button:", selectedOption)
+
+    fetchExpenseTrendData(selectedOption);
+  })
+
+});
--- billstracker/urls.py
+++ billstracker/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import LoginPage, MainPage, BillList, BillDetailView, DeleteBill, CreateBill, UpdateBill, PayBill, BillView, DeleteBillDetail, CreateCategory
 from .views import ProfileView, PasswordChange, AccountView, MoneyAccountList, MoneyAccountAdd, MoneyAccountView, MoneyAccountDelete, MoneyAccountUpdate
 from .views import MoneyTransfer, TransactionHistory, AccountGroupList, AccountGroupAdd, AccountGroupDelete, AccountGroupView, AccountGroupEdit, MoneyIncome
-from .views import MoneyExpense, AdminView, FeedbackCreate
+from .views import MoneyExpense, AdminView, FeedbackCreate, MonthlyExpenseDataView, IncomeExpenseDataView, ExpenseTrendDataView
 
 from django.contrib.auth.views import LogoutView
 from django.views.generic.base import RedirectView
@@ -10,8 +10,8 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('login/', LoginPage.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('', MainPage.as_view(), name='main'),
     
-    path('', RedirectView.as_view(url='/bills-tracker/', permanent=False), name='main'),
     path('bills-tracker/', BillList.as_view(), name='bills-tracker'),
     path('bills-tracker/billdetail/<int:pk>', BillDetailView.as_view(), name='billdetail-view'),
     path('bills-tracker/bill/<int:pk>', BillView.as_view(), name='bill-view'),
@@ -21,8 +21,7 @@ urlpatterns = [
     path('bills-tracker/bill/pay-bill/<int:b_id>/<int:d_id>', PayBill.as_view(), name='pay-bill'),
     path('bills-tracker/billdetail/delete-billdetail/<int:pk>', DeleteBillDetail.as_view(), name='billdetail-delete'),
     path('bills-tracker/bill/createcategory/', CreateCategory.as_view(), name='create-category'),
-    path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
-    path('profile/passwordchange/<int:pk>', PasswordChange.as_view(), name='password-change'),
+    
     path('money-tracker/dashboard', AccountView.as_view(), name='money-dashboard'),
     path('money-tracker/accounts', MoneyAccountList.as_view(), name='money-accounts'),
     path('money-tracker/accounts/add', MoneyAccountAdd.as_view(), name='money-accounts-add'),
@@ -38,6 +37,13 @@ urlpatterns = [
     path('money-tracker/account-groups/<int:pk>/delete', AccountGroupDelete.as_view(), name='money-account-group-delete'),
     path('money-tracker/accounts/income', MoneyIncome.as_view(), name='money-accounts-income'),
     path('money-tracker/accounts/expense', MoneyExpense.as_view(), name='money-accounts-expense'),
+    
     path('profile/admin/', AdminView.as_view(), name='profile-admin'),
     path('profile/feedback/new', FeedbackCreate.as_view(), name='feedback-new'),
+    path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
+    path('profile/passwordchange/<int:pk>', PasswordChange.as_view(), name='password-change'),
+    
+    path('get-monthly-expense-data/', MonthlyExpenseDataView.as_view(), name='get-monthly-expense-data'),
+    path('get-income-expense-data/', IncomeExpenseDataView.as_view(), name='get-income-expense-data'),
+    path('get-expense-trends-data/', ExpenseTrendDataView.as_view(), name='get-expense-trend-data'),
 ]
